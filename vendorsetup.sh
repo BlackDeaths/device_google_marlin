622487810798b628a058d87d582459abca0eaa95
BlackDeath <BlackDeaths@users.noreply.github.com>
2018-12-11 12:10:38 +0100

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -31,4 +31,4 @@ add_lunch_combo aosp_marlin-userdebug
 add_lunch_combo aosp_marlin_svelte-userdebug
 add_lunch_combo aosp_sailfish-userdebug
 add_lunch_combo bootleg_marlin-userdebug
-add_lunch_combo bootleg_sailfish-userdebug
+add_lunch_combo lineage_sailfish-userdebug
