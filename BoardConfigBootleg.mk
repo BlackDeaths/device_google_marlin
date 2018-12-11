fb0974a33a1609c67873acb0b3e5c07f376186db
BlackDeath <BlackDeaths@users.noreply.github.com>
2018-12-11 12:12:29 +0100

    Update BoardConfigBootleg.mk

--- BoardConfigBootleg.mk
+++ BoardConfigBootleg.mk
@@ -1,7 +1,7 @@
 # Custom Bootleggers BoardConfig for marlin/sailfish
 
 # Inline kernel building
-TARGET_KERNEL_CONFIG := bootleg_defconfig
+TARGET_KERNEL_CONFIG := lineageos_marlin_defconfig
 BOARD_KERNEL_IMAGE_NAME := Image.lz4-dtb
 TARGET_COMPILE_WITH_MSM_KERNEL := true
 TARGET_KERNEL_SOURCE := kernel/google/marlin
